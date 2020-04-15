15d121c3f200493c8b5b8c655b7e218d267c3ea9
Jared S <jaredjames2020@gmail.com>
2020-04-15 21:24:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Your code here
-function saturdayFun(thing = "roller-skate") {
-  return `This Saturday, I want to ${thing}!`;
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+  return `This Monday, I will ${activity}`
 }
