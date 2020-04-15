a675f8bc73e203954c9583032ec22e2c2ea63a51
Jared S <jaredjames2020@gmail.com>
2020-04-15 21:52:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function mondayWork(activity = "go to the office"){
 }
 
 function wrapAdjective(flair = "*") {
-  return function(adjective = "special") {
+  return function(adjective = 'special') {
     return `You are ${flair}${param1}${flair}!`;
   };
 }
