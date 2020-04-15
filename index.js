cd21e46340bcc304ef43d57a5383641684740a1c
Jared S <jaredjames2020@gmail.com>
2020-04-15 21:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function mondayWork(activity = "go to the office"){
 
 function wrapAdjective(flair = '*') {
   return function(adjective = "special") {
-    return `You are ${flair}${param1}!`
+    return `You are ${flair}${param1}!`;
   };
 }
+let encouragingPromptFunction = wrapAdjective("!!!");
